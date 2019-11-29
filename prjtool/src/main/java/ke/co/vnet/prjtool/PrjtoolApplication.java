ac88345262c88dad76573684eea26526bd973e4a
Elliy <sammurira@gmail.com>
2019-11-29 21:27:15 +0300

    first commit

---
+++ prjtool/src/main/java/ke/co/vnet/prjtool/PrjtoolApplication.java
@@ -0,0 +1,13 @@
+package ke.co.vnet.prjtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjtoolApplication.class, args);
+    }
+
+}
---
+++ prjtool/src/test/java/ke/co/vnet/prjtool/PrjtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.co.vnet.prjtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
