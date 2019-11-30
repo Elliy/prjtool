1a0965e53a732689b115acd2a4d6dd4eac268558
Elliy <sammurira@gmail.com>
2019-11-30 10:40:37 +0300

    handling unique constraint

---
+++ prjtool/src/main/java/ke/co/vnet/prjtool/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,19 @@
+package ke.co.vnet.prjtool.exceptions;
+
+public class ProjectIdExceptionResponse {
+
+    private String projectIdentifier;
+
+
+    public ProjectIdExceptionResponse(String projectIdentifier){
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+}
--- prjtool/src/main/java/ke/co/vnet/prjtool/web/ProjectController.java
+++ prjtool/src/main/java/ke/co/vnet/prjtool/web/ProjectController.java
@@ -31,13 +31,7 @@ public class ProjectController {
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
-
-
-
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
-
-
-
 }
