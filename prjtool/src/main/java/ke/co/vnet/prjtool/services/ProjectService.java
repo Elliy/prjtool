45b5aaf61dc80b0fc2dbb56c8f90aa3dc4ff36b1
Elliy <sammurira@gmail.com>
2019-12-01 21:26:57 +0300

    Delete operation - project

--- prjtool/src/main/java/ke/co/vnet/prjtool/services/ProjectService.java
+++ prjtool/src/main/java/ke/co/vnet/prjtool/services/ProjectService.java
@@ -34,4 +34,14 @@ public class ProjectService {
     public Iterable<Project> findAllProjects(){
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier((projectId.toUpperCase()));
+
+        if (project == null){
+            throw  new ProjectIdException(("Project with ID '"+projectId+ "'. Does not exist"));
+        }
+
+        projectRepository.delete(project);
+    }
 }
